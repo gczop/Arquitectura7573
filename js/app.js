6d5caf319061bc14cdbd4c7f08e513923d3ada0e
Rodri Valdivia <rodrivaldivia10@gmail.com>
2018-10-06 14:41:13 -0300

    added timeout endpoint to node

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ app.get("/", (req,res) => {
 	res.send("Hello There\n");
 })
 
+app.get("/timeout", (req,res) => {
+	setTimeout(function() {
+		res.send("General Kenobi\n");
+	}, 1000);
+})
+
 
 app.listen(PORT, () => {
 	console.log("Listening on ", PORT);
