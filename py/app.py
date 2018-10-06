5046ac54bbfb579f15bcaea3962cf75441287caa
gczop <guilleczop@gmail.com>
2018-10-06 00:23:33 -0300

    Agregado timeout de python

--- py/app.py
+++ py/app.py
@@ -1,4 +1,5 @@
 from flask import Flask;
+import time
 
 
 app = Flask(__name__);
@@ -11,6 +12,11 @@ def root():
 @app.route("/hola")
 def hola():
 	return "Looser\n";
+
+@app.route("/timeout")
+def timeout():
+	time.sleep(10)
+	return "Ping"
 
 
 if __name__ == "__main__":
